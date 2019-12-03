6826c5f0500a4dbed19c3fb92f43e83141405e98
BergurThor <bergura@gmail.com>
2019-12-03 15:43:48 +0000

    classes for flight and destination created

---
+++ DataLayer/destination.py
@@ -0,0 +1,14 @@
+class Destination:
+
+    def __init__(self, destinationID, country, airport, flightDuration, distanceFromIceland, contactName, contactNumber):
+        
+        self.destinationID = destinationID
+        self.country = country
+        self.airport = airport
+        self.flightDuration = flightDuration
+        self.distanceFromIceland = distanceFromIceland
+        self.contactName = contactName
+        self.contactNumber = contactNumber
+
+    def __str__(self):
+        return self.country + " " + self.airport + " " + self.flightDuration
