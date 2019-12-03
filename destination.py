fe988503500cf009e7fc2bf5a4e805b9f3e085ae
ari <arig19@ru.is>
2019-12-03 15:43:52 +0000

    added private variables to the class

--- destination.py
+++ destination.py
@@ -1,3 +1,10 @@
 class destination:
-    def __init__(self, ID, country, airport, flightDuration, distanceFromIceland, contactName, contactNumber):
-        pass
+    def __init__(self, destination_ID, country, airport, flightDuration, distanceFromIceland, contactName, contactNumber):
+        self.__destination_ID = destination_ID
+        self.__country = country
+        self.__airport = airport
+        self.__flightDuration = flightDuration
+        self.__distanceFromIceland = distanceFromIceland
+        self.__contactName = contactName
+        self.__contactNumber = contactNumber
+        
