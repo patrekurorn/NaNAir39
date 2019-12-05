c379d0ed368eb433a948df6682ec74c2d40fe851
Patrekur Örn <patrekurf19@ru.is>
2019-12-05 15:36:16 +0000

    drasl

--- DataProcessing/employee_data.py
+++ DataProcessing/employee_data.py
@@ -20,11 +20,8 @@ def main():
         landlineNr_int = row[8]
         email_str = row[9]
         data.append(Employee(ssn_int, name_str, position_str, rank_str, licence_str, address_str, mobile_int, landlineNr_int, email_str))
-
-
+        
         #print(row)
-        employee_a = Employee(ssn, name, position, rank, licence, address, mobile, landlineNr, email)
+        employee_a = Employee(ssn_int, name_str, position_str, rank_str, licence_str, address_str, mobile_int, landlineNr_int, email_str)
         employee_a.list_all_pilots()
-        
-        
 main()
