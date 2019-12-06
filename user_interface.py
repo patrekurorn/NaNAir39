d731ad59544eb27ebcdae612a3a5f121223c85a1
BergurThor <bergura@gmail.com>
2019-12-06 10:43:45 +0000

    user_interface broken #notkeepingitclean

--- user_interface.py
+++ user_interface.py
@@ -3,13 +3,19 @@
 class User:
 
     def __init__(self):
+
+        #self.header = get_header()
+        #self.footer = get_footer()
         pass
 
     def home(self):
         choice = ""
-
+        print(get_header())
         print("1. Staff manager")
         print("2. Planning manager")
+        print(get_footer())
+
+        choice = input().lower()
 
         if choice == "1":
             print("1. Man flights")
@@ -20,3 +26,24 @@ class User:
             print("2. List of voyages")
             print("3. Manage destinations")
 
+    
+    def get_header(self):
+        return ("___________________________________________________________\n" + \
+                "                        NaN Air                            \n" + \
+                "___________________________________________________________\n" + \
+                "-----------------------------------------------------------\n" + \
+                "\n")
+
+
+    def get_footer(self):
+        return ("-----------------------------------------------------------\n" + \
+                "___________________________________________________________\n" + \
+                "\n" + \
+                "                          _|_                              \n" + \
+                "                   *---o--(_)--o---*                       \n" + \
+                "___________________________________________________________")
+
+
+user = User()
+
+print(user.get_header())
