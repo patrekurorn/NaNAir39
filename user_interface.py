501cf947922a3f346e1aceff74e404b0482e6643
kumasi <Kumasimani19@gmail.com>
2019-12-06 12:27:57 +0000

    Uppfært margt í user_interface

--- user_interface.py
+++ user_interface.py
@@ -1,65 +1,55 @@
 VALID = ["1","2","3"]
 
 class User:
+    def __init__(self,first_pick = None, second_pick = None):
+        self.first_pick = first_pick
+        self.second_pick = second_pick
 
-    def __init__(self):
+    def __repr__(self):
+        return User()
 
-        #self.header = get_header()
-        #self.footer = get_footer()
-        pass
+    def __str__(self):
+        return "{} {}".format(self.first_pick,self.second_pick)
 
     def home(self):
-      #  choice = ""
         userInput_str = input("1. Staff manager \n2. Planning manager \nYour pick: ")
-        if userInput_str == "1":
-            return "1"
-        elif userInput_str == "2":
-            return "2"
+        if userInput_str == "1" or userInput_str == "2":
+            self.first_pick = userInput_str
+            return self.first_pick
+
         else:
             print("Wrong input")
             return user.home()
 
-       # print(get_feed())
-      #  print(get_header())
-      #  print("1. Staff manager")
-      #  print("2. Planning manager")
-      #  print(get_footer())
+    def second_window(self):
 
-   #     choice = input().lower()
-   #     def choice1(self):
-
-    def pick(self,pick):
-        def first_pick(pick):
-            pick_input = input("1. Man flights \n2. List of work force\n3. Manage work force")
+        def first_pick():
+            pick_input = input("1. Man flights \n2. List of work force\n3. Manage work force\nYour pick: ")
             if pick_input not in VALID:
                 print("wrong input")
-                return first_pick(pick)
+                return first_pick()
             else:
+                self.second_pick = pick_input
                 return pick_input
 
-        def second_pick(pick):
-            pick_input = input("1. Manage voyages\n2. List of voyages\n3. Manage destinations")
+
+        def second_pick():
+            pick_input = input("1. Manage voyages\n2. List of voyages\n3. Manage destinations\Your pick: ")
             if pick_input not in VALID:
                 print("wrong input")
-                second_pick(pick)
+                return second_pick()
             else:
+                self.second_pick = pick_input
                 return pick_input
 
-        if pick == "1":
-            first_pick(pick)  
-        elif pick == "2":
-            second_pick(pick)
 
-   #     if choice == "1":
-   #         print("1. Man flights")
-   #         print("2. List of work force")
-   #         print("3. Manage work force")
+        if self.first_pick == "1":
+            first_pick()
 
-   #     def choice2)
-   #     if choice == "2":
-   #         print("1. Manage voyages")
-   #         print("2. List of voyages")
-   #         print("3. Manage destinations")
+        elif self.first_pick  == "2":
+            second_pick()
+
+
 
     
     def get_header(self):
@@ -81,9 +71,11 @@ class User:
 
 user = User()
 user.get_header()
-user_pick = user.home()
+user_selection = user.home()
 user.get_footer()
-next_selection = user.pick(user_pick)
+
+user.second_window()
+print(user)
 
 
 
