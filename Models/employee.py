82acea481b4d534177113e139234effbce5d0ab6
Patrekur Örn <patrekurf19@ru.is>
2019-12-06 00:35:58 +0000

    new shit!!!!!

--- Models/employee.py
+++ Models/employee.py
@@ -21,52 +21,71 @@ class Employee:
     """ SSN """
     def get_ssn(self):
         return self.ssn
-    def set_ssn(self):
-        self.ssn = self
+
+    def set_ssn(self, ssn):
+        self.ssn = ssn
+
     """ Name """
     def get_name(self):
         return self.name
-    def set_name(self):
-        self.name = self
+
+    def set_name(self, name):
+        self.name = name
+
     """ Role """
     def get_position(self):
         return self.position
-    def set_position(self):
-        self.position = self
+
+    def set_position(self, position):
+        self.position = position
+
     """ Rank """
     def get_rank(self):
         return self.rank
-    def set_rank(self):
-        self.rank = self
+
+    def set_rank(self, rank):
+        self.rank = rank
+
     """ Licence """
     def get_licence(self):
         return self.licence
-    def set_licence(self):
-        self.licence = self
+
+    def set_licence(self, licence):
+        self.licence = licence
+
     """ Address """
     def get_address(self):
         return self.address
-    def set_address(self):
-        self.address = self
+
+    def set_address(self, address):
+        self.address = address
+
     """ Postion """
     def get_position(self):
         return self.position
-    def set_position(self):
-        self.position = self
+
+    def set_position(self, position):
+        self.position = position
+
     """ Landline Nr """
     def get_landlineNr(self):
         return self.landlinenr
-    def set_landlineNr(self):
-        self.landlinenr = self
+
+    def set_landlineNr(self, landlineNr):
+        self.landlinenr = landlineNr
+
     """ Email """
     def get_email(self):
         return self.email
-    def set_email(self):
-        self.email = self
+
+    def set_email(self, email):
+        self.email = email
+
     """ Mobile """
     def get_mobile(self):
         return self.mobile
-    def set_mobile(self):
-        self.mobile = self
+
+    def set_mobile(self, mobile):
+        self.mobile = mobile
         
     
--- Models/voyage.py
+++ Models/voyage.py
@@ -3,14 +3,14 @@
 class Voyage:
 
     def __init__(self, flightNumber, departingFrom, arrivingAt, departure, arrival):
-        self.flightNumber = int(flightNumber)
+        self.flightNumber = flightNumber
         self.departingFrom = departingFrom
         self.arrivingAt = arrivingAt
         self.departure = departure
         self.arrival = arrival
 
     def __str__(self):
-        return "Flight number: {}\nDeparting from: {}\nArriving at: {}\nDeparture: {}\nArrival: {}\n".format(self.flightNumber, self.departingFrom, self.arrivingAt, self.departure, self.arrival)
+        return "{} {} {} {} {}".format(self.flightNumber, self.departingFrom, self.arrivingAt, self.departure, self.arrival)
     
     def get_flightNumber(self):
         return self.flightNumber
