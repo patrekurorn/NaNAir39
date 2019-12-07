8699bd7c147c80dd62f86d6ee931267c2692fcf7
kumasi <Kumasimani19@gmail.com>
2019-12-07 10:44:51 +0000

    uppfært Airplane í Models()

--- Models/airplane.py
+++ Models/airplane.py
@@ -1,26 +1,43 @@
 class Airplane:
 
     def __init__(self, planeInsignia, planeTypeId, manufacturer, model, capacity):
-        self.__planeInsignia = planeInsignia
-        self.__planeTypeId = planeTypeId
-        self.__manufacturer = manufacturer
-        self.__model = model
-        self.__capacity = int(capacity)  # passenger seats
+        self.planeInsignia = planeInsignia
+        self.planeTypeId = planeTypeId
+        self.manufacturer = manufacturer
+        self.model = model
+        self.capacity = int(capacity)  # passenger seats
 
-    def get_planeInsignia(self):
-        return self.__planeInsignia
+    """Getters"""
+    def get_plane_insignia(self):
+        return self.planeInsignia
 
-    def get_planeTypeId(self):
-        return self.__planeTypeId
+    def get_plane_type_id(self):
+        return self.planeTypeId
 
     def get_manufacturer(self):
-        return self.__manufacturer
+        return self.manufacturer
 
     def get_model(self):
-        return self.__model
+        return self.model
 
     def get_capacity(self):
-        return self.__capacity
+        return self.capacity
+
+    """Setters"""
+    def set_plane_insignia(self,other):
+        self.planeInsignia = other
+
+    def set_plane_type_id(self,other):
+        self.planeTypeId = other
+
+    def set_manufacturer(self,other):
+        self.manufacturer = other
+
+    def set_model(self,other):
+        self.model = other
+
+    def set_capacity(self,other):
+        self.capacity = other
 
     def __str__(self):
         return "Plane insignia: {}\nPlane Type ID: {}\nPlane Type ID: {}\nManufacturer: {}\nModel: {}\nCapacity: {}".format(self.__planeInsignia, self.__planeTypeId, self.__planeTypeId, self.__manufacturer, self.__model, self.__capacity)
